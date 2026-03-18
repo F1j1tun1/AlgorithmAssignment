2bc37ce506dee42783de9474bc86056f9b255458
F1j1tun1 <erkingalisaruar@gmail.com>
2026-03-18 17:54:06 +0500

    init commit

--- Tasks/task9.java
+++ Tasks/task9.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class task9 {
+    public static int counting(String n, int i, int count) {
+        if (i == n.length()) {
+            return count;
+        }
+        count++;
+        return counting(n, i + 1, count);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String n = sc.next();
+        int count = 0;
+        System.out.println(counting(n, 0, count));
+    }
 }
