148f3356ed07b063cda2490a65a3988a0683ce04
F1j1tun1 <erkingalisaruar@gmail.com>
2026-03-18 14:55:17 +0500

    init commit

--- Tasks/task.java
+++ Tasks/task.java
@@ -1,14 +1,26 @@
 import java.util.Scanner;
 
 public class task {
-    public static void function(int n){
-        if(n == 0) return;
-        function(n / 10);
-        System.out.println(n % 10);
+    public static void fillArray(int[] arr, int i, Scanner sc) {
+        if (i == arr.length) return;
+        arr[i] = sc.nextInt();
+        fillArray(arr, i + 1, sc);
+    }
+    public static double sum(int[] arr, int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        return arr[n - 1] + sum(arr, n - 1);
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        function(n);
+        int[] numbers = new int[n];
+
+        fillArray(numbers, 0, sc);
+        double result = sum(numbers, n);
+        double average = result / n;
+
+        System.out.println(average);
     }
 }
