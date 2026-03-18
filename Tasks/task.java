c22dfe8a6f4166c3cf9382aec611a0cd54768e25
F1j1tun1 <erkingalisaruar@gmail.com>
2026-03-18 13:24:23 +0500

    init commit

--- Tasks/task.java
+++ Tasks/task.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class task {
+    public static void function(int n){
+        if(n == 0) return;
+        function(n / 10);
+        System.out.println(n % 10);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        function(n);
+    }
 }
