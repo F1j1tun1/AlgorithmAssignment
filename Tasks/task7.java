264736b8753744024e40ffb06064ef6c78543f1f
F1j1tun1 <erkingalisaruar@gmail.com>
2026-03-18 16:48:26 +0500

    init commit

--- Tasks/task7.java
+++ Tasks/task7.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class task7 {
+    public static void flip(int n, Scanner sc) {
+        if (n == 0) {
+            return;
+        }
+        int a = sc.nextInt();
+        flip(n - 1, sc);
+        System.out.println(a + " ");
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        flip(n, sc);
+    }
 }
