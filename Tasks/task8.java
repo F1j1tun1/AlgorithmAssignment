8afd5e3ce0c30ca452df89e2c18f9e649e6f40db
F1j1tun1 <erkingalisaruar@gmail.com>
2026-03-18 17:29:26 +0500

    init commit

--- Tasks/task8.java
+++ Tasks/task8.java
@@ -1,7 +1,18 @@
 import java.util.Scanner;
 
 public class task8 {
+    public static String digit(String n, int i) {
+        if (i == n.length()) {
+            return "Yes";
+        }
+        else if (!Character.isDigit(n.charAt(i))) {
+            return "No";
+        }
+        return digit(n, i + 1);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String n = sc.next();
+        System.out.println(digit(n, 0));
     }
 }
